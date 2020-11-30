ddd07813d36115e6d71a7608eb6c04f9c89435e9
skylarskylar <70194012+skylarskylar@users.noreply.github.com>
2020-11-30 13:12:30 -0800

    from hub

--- CoronaSolver.java
+++ CoronaSolver.java
@@ -1,4 +1,27 @@
 
 public class CoronaSolver {
+	//Returns a string of Ls and Ns to designate which days to spend
+
+	//in Los Angeles and New York City such that we will be exposed
+
+	//to as few infected people as possible.  inputFileName’s file
+
+	//will be in the following format:
+
+	//The first line will be the total number of days, n.
+
+	// The second line will be the number of people you’re exposed to
+
+	// on a flight, F.
+
+	//Every subsequent line will have two numbers, separated by //spaces:
+
+	// Li Ni
+
+	public String solve(String inputFileName) {
+		
+		
+	}
+
 
 }
