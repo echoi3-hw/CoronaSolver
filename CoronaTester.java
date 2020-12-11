9b35fb703c08e9b187c7eb14899e8f730790dcd9
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-12-11 10:41:05 -0800

    Update CoronaTester.java

--- CoronaTester.java
+++ CoronaTester.java
@@ -1,4 +1,9 @@
-
+import java.io.IOException;
 public class CoronaTester {
-
+public static void main (String [] args) throws IOException
+{
+	CoronaSolver test = new CoronaSolver ("");
+	test.solve();
+}
+	
 }
